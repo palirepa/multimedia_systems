05ea645ab666af772dd392a637865ecd5d603af5
Flopity Flops <99768688+palirepa@users.noreply.github.com>
2022-10-04 10:45:14 +0200

    Update BasicController.java

--- uvod/src/main/java/mds/uvod/BasicController.java
+++ uvod/src/main/java/mds/uvod/BasicController.java
@@ -1,52 +1,72 @@
 package mds.uvod;
 
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("basic")
-
 public class BasicController {
 
-
     @GetMapping
-    public String testBasic() {
-        return "<b>Hello from test</b>";
+        public String testBasic(){
+        return "<h4><b>Hello world!</b></h4>";
     }
 
     @GetMapping("list")
-    public List<String> testList() {
+    public List<String> testList(){
         return List.of("Hello", "world", "in", "JSON");
     }
 
-    @GetMapping("param1")
-    public String testParam1(@RequestParam(defaultValue = "user") String name) {
-        return String.format("Hello %s", name);
+    @GetMapping("test1")
+    public String testParam1(@RequestParam String name){
+        return String.format("Hello <b>%s</b>! Welcome to our page.", name);
     }
 
-    @GetMapping("param2")
-    public String testParam2(@RequestParam(defaultValue = "user", name = "jmeno") String name2) {
-        return String.format("Hello %s", name2);
+    @GetMapping("test2")
+    public String testParam2(@RequestParam(defaultValue = "user") String name){
+        return String.format("Hello <b>%s</b>! Welcome to our page.", name);
     }
 
-    @GetMapping("param3")
-    public String testParam3(@RequestParam List<String> id) {
-        return "All IDs are: " + id;
+    @GetMapping("test3")
+    public String testParam3(@RequestParam(name = "n", defaultValue = "user") String name){
+        return String.format("Hello <b>%s</b>! Welcome to our page.", name);
     }
 
+    @GetMapping("test4")
+    public String testParam4(@RequestParam(defaultValue = "user") String name,
+                             @RequestParam(defaultValue = "-1")int id){
+        return String.format("Hello <b>%s</b>! Welcome to our page. Your ID is: %d", name, id);
+    }
+
+    @GetMapping("test5")
+    public String testParam5(@RequestParam List<String> id){
+        //return "All IDs are:" + id;
+
+        String w = "";
+        for(String s : id){
+            w += s + "<br>";
+        }
+        return w;
+    }
 
     @GetMapping("form")
-    public String helloForm() {
-
-        return "<html>" + "<body>" + "<form method='post' action='hello'>" + "<input type='text' name='name'/>" + "<input type='submit' value='Pozdrav'/>" + "</form>" + "</body>" + "</html>";
-    }
-
-
-    @RequestMapping(value = "hello", method = {RequestMethod.POST, RequestMethod.GET})
-    public String helloTest(String name) {
-        return String.format("Hello %s", name);
+    public String helloForm(){
+        String html =
+                "<html>" +
+                        "<body>" +
+                            "<form method='post' action='hello'>" +
+                                "<input type = 'text' name='name'/>" +
+                                "<input type = 'submit' value='Pozdrav!'/>" +
+                            "</form>" +
+                        "</body>" +
+                "</html>";
+        return html;
     }
 
 }
-
