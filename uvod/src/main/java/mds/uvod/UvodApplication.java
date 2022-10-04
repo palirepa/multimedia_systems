83e7494c4aaef833ef63fa41c48580c7734d90a1
Flopity Flops <99768688+palirepa@users.noreply.github.com>
2022-10-04 10:45:32 +0200

    Update UvodApplication.java

--- uvod/src/main/java/mds/uvod/UvodApplication.java
+++ uvod/src/main/java/mds/uvod/UvodApplication.java
@@ -2,22 +2,21 @@ package mds.uvod;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
-
 @SpringBootApplication
+@Controller
 public class UvodApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UvodApplication.class, args);
 	}
-		@GetMapping
-		@ResponseBody
-	public String hello() {
-		return "Hello World";
-		}
 
-	ArrayList<Student> students = new ArrayList<>();
+	@GetMapping
+	@ResponseBody
+	public String hello(){
+		return "<h1><b>Hello world!</b></h1>";
+	}
 }
